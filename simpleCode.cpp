b8421eb5ebc04dea6087f44a016cb87a6029e1fb
Mahan Sadati <sadatimahan83@gmail.com>
2024-12-21 17:33:43 +0330

    project created

---
+++ simpleCode.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+main(){
+    int a , b;
+    cout << "enter a:"; // input a variable
+    cin >> a;
+    cout << "enter b:"; // input b variable
+    cin >> b;
+    cout << "a + b = " << a + b << endl;
+    cout << "a * 2 = " << a * 2 ;       // outputs
+    getch();
+}
